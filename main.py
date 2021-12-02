2cdd4b4be8098ce6646b6ca11dc5b3aaf2f34946
Raghav Bakshi <raghavbakshi19@gmail.com>
2021-12-02 19:29:07 +0530

    changes

--- main.py
+++ main.py
@@ -1,19 +1,56 @@
+from flask import Flask, request, render_template
+from flask import Response
+from flask_cors import cross_origin
 from training_validation_insertion import Pred_validation
 from train_model import trainModel
 from prediction_validation_insertion import Pred_validations
 from prediction_fromModel import prediction
 
-pred_val = Pred_validation(r"E:\full stack\internship\network security\dataset")
-pred_val.prediction_validation()
+app = Flask(__name__)
 
-train = trainModel()
-train.trainingModel()
 
-preds_val = Pred_validations(r"E:\full stack\internship\network security\prediction_file")
-preds_val.prediction_validation()
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template('login.html')
 
-pred = prediction(r"E:\full stack\internship\network security\Prediction_FileFromDB\InputFile.csv")
-pred.predictionFromModel()
 
+@app.route("/", methods=['POST'])
+@cross_origin()
+def predictRouteClient():
+    if request.method == 'POST':
+        try:
+            path = request.form['Prediction Path']
+            preds_val = Pred_validations(r"E:\full stack\internship\network security\prediction_file")
+            preds_val.prediction_validation()
+            # path = "Prediction_FileFromDB\InputFile.csv"
+            pred = prediction(path)
+            pred.predictionFromModel()
+
+        except ValueError:
+            return Response("Error Occurred! %s" % ValueError)
+        except KeyError:
+            return Response("Error Occurred! %s" % KeyError)
+        except Exception as e:
+            return Response("Error Occurred! %s" % e)
+
+    return render_template('results.html')
+@app.route("/train", methods=['POST'])
+@cross_origin()
+def trainRouteClient():
+    try:
+
+
+        pred_val = Pred_validation(r"E:\full stack\internship\network security\dataset")  # object initialization
+        pred_val.prediction_validation()  # calling the training_validation function
+
+        train = trainModel()  # object initialization
+        train.trainingModel()  # training the model for the files in the table
+
+
+
+    except ValueError:
+
+        return Response("Error Occurred! %s" % ValueError)
 
 
